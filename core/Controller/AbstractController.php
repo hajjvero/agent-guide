6748068719bebbd3558d918032e22278b7de3c9e
Hamza Hajjaji <hh.hajjaji.hamza@gmail.com>
2026-01-05 19:01:42 +0100

    feat(controller): ajouter une classe de contrôleur abstraite avec des méthodes de rendu de vue
    - Implémentation de la méthode renderView pour générer le contenu d'une vue sous forme de chaîne
    - Ajout de la méthode render pour afficher directement le contenu d'une vue
    - Gestion des chemins de template avec extension .php automatique
    - Vérification de l'existence du fichier template avant le rendu
    - Extraction des données dans le contexte de la vue
    - Documentation complète des méthodes avec annotations PHPDoc

---
+++ core/Controller/AbstractController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Core\Controller;
+
+
+use RuntimeException;
+
+abstract class AbstractController
+{
+    /**
+     * Renders a view and returns its content as a string.
+     *
+     * @param string $path The path to the view relative to the template directory (e.g., 'home/index' ou 'home/index.php').
+     * @param array $data The data to pass to the view.
+     * @return string
+     * @throws RuntimeException If the template file is not found.
+     */
+    protected function renderView(string $path, array $data = []): string
+    {
+        $templatePath = dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'template' . DIRECTORY_SEPARATOR . ltrim($path,DIRECTORY_SEPARATOR);
+
+        if (!str_ends_with($templatePath, '.php')) {
+            $templatePath .= '.php';
+        }
+
+        if (!file_exists($templatePath)) {
+            throw new RuntimeException("Template file not found: {$templatePath}");
+        }
+
+        extract($data);
+
+        ob_start();
+        require $templatePath;
+        return ob_get_clean();
+    }
+
+    /**
+     * Renders a view and outputs its content.
+     *
+     * @param string $path The path to the view relative to the template directory.
+     * @param array $data The data to pass to the view.
+     * @return void
+     */
+    protected function render(string $path, array $data = []): void
+    {
+        echo $this->renderView($path, $data);
+    }
+}
