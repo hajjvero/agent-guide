59bc60a3afb261e287e3232b7233e82bea82bb58
nouh <nouh.nowile@gmail.com>
2026-01-06 09:49:37 +0100

    create the controler for all entities

---
+++ src/Controller/Admin/ProductController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Core\Controller\AbstractController;
+use Core\Route\Attribute\Route;
+
+#[Route(path: '/product', name: 'product.')]
+class ProductController extends AbstractController
+{
+    #[Route(path: '/', name: 'index')]
+    public function index(): void
+    {
+        $this->render('admin/product/index');
+    }
+
+    #[Route(path: '/create', name: 'create')]
+    public function create(): void
+    {
+        $this->render('admin/product/create');
+    }
+
+    #[Route(path: '/store', name: 'store', methods: 'POST')]
+    public function store(): void
+    {
+    }
+
+    #[Route(path: '/{id}/edit', name: 'edit')]
+    public function edit(int $id): void
+    {
+        $this->render('admin/product/edit');
+    }
+
+    #[Route(path: '/{id}/update', name: 'update', methods: 'POST')]
+    public function update(int $id): void
+    {
+    }
+
+    #[Route(path: '/{id}/delete', name: 'delete', methods: 'POST')]
+    public function delete(int $id): void
+    {
+    }
+}
